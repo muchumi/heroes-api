50fe94eb39de4d086d05310b281fe36e18e82ac9
muchumi <muchumimaina94@gmail.com>
2024-01-01 08:22:12 +0300

    Setting up database and http_status_codes

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 export FLASK_ENV=development
 export FLASK_APP=api
+export SQLALCHEMY_DATABASE_URI=sqlite:///heroes.db
--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import os
 from api.views.users import users
 from api.views.heroes import heroes
+from api.database import db
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -9,10 +10,13 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
         )
     else:
         app.config.from_mapping(test_config)
 
+    db.app=app
+    db.init_app(app)
     app.register_blueprint(users)
     app.register_blueprint(heroes)
 
