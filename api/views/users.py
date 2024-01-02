6fab6d31d9f6b9c751d1e613a3338a5501636583
muchumi <muchumimaina94@gmail.com>
2024-01-02 05:50:47 +0300

    Created the user registration endpoint

--- api/views/users.py
+++ api/views/users.py
@@ -1,10 +1,62 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from api.constants.http_status_codes import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_201_CREATED
+import validators
+from api.database import db
+from api.models.users import User
 
 users = Blueprint("users", __name__, url_prefix="/api/v1/users")
 
 @users.route("/register", methods=['POST'])
 def register():
-    return "User created"
+    firstName=request.json["firstName"]
+    lastName=request.json["lastName"]
+    userName=request.json["userName"]
+    email=request.json["email"]
+    password=request.json["password"]
+
+    if len(password)<8:
+        return jsonify({
+            'error': "Password is too short"
+        }), HTTP_400_BAD_REQUEST
+    
+    if len(userName)<5:
+        return jsonify({
+            'error': "Username is too short"
+        }), HTTP_400_BAD_REQUEST
+    
+    if not userName.isalnum() or " " in userName:
+        return jsonify({
+            'error': "Username should be alphanumeric and no spaces"
+        }), HTTP_400_BAD_REQUEST
+    
+    if not validators.email(email):
+        return jsonify({
+            'error': "Email is not valid"
+        }), HTTP_400_BAD_REQUEST
+
+    if User.query.filter_by(email=email).first() is not None:
+        return jsonify({
+            'error': "Email is taken"
+        }), HTTP_409_CONFLICT
+    
+    if User.query.filter_by(userName=userName).first() is not None:
+        return jsonify({
+            'error': "Username is taken"
+        }), HTTP_409_CONFLICT
+    
+    password_hash=generate_password_hash(password)
+
+    user=User(firstName=firstName, lastName=lastName, userName=userName, email=email, password=password_hash)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({
+        'message': "User created successfully",
+        'user': {
+            'username': userName,
+            'email': email
+        }
+    }), HTTP_201_CREATED
 
 @users.route("/me", methods=['GET'])
 def me():
